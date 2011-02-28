72ac861c13e3bbe99baf038ed66d291474239b1f
Alberto Morales <alberto@moralitos.com>
2011-02-28 02:19:40 -0500

    changing coordinates

--- trebuchet/lever.rb
+++ trebuchet/lever.rb
@@ -4,19 +4,42 @@ class Lever
     @trebuchet = trebuchet
     @game_window = @trebuchet.game_window
     @angle = 124
-    @thickness = 24
-    @center_x = 350
-    @center_y = 400
+    @thickness = 40
+    @center_x = 355
+    @center_y = 350
     @radius = 200.0
-    @color = Color.new.yellow
+    @color = Color.new
+    @lever_color = @color.red
+    @axle_color = @color.green
+    @layer = 3
   end
   
   def draw
-    @game_window.draw_line(x1+@center_x,y1-@center_y,@color,x2+@center_x,y2-@center_y,@color,2)
+    draw_lever
+    draw_axle
+  end
+  
+  def draw_axle
+    @game_window.draw_quad(
+      @center_x-5, @game_window.height - @center_y-5,@color.red,
+      @center_x+5,@game_window.height - @center_y-5, @color.red,
+      @center_x-5,@game_window.height - @center_y+5,@color.red,
+      @center_x+5,@game_window.height - @center_y+5,@color.red,
+      @layer+10)
+  end
+  
+  def draw_lever
+    @game_window.draw_quad(
+      x1,y1,@color.green,
+      x2,y2, @color.green,
+      x3,y3,@color.green,
+      x4,y4,@color.green,
+      @layer)
   end
   
   def update
-    puts @angle
+    puts "x1: #{x1},x2: #{x2}, x3: #{x3}, x4: #{x4}"
+    puts "y1: #{y1},y2: #{y2}, y3: #{y3}, y4: #{y4}"
     if @angle < 360
       @angle = @angle +1
     else
@@ -24,20 +47,40 @@ class Lever
     end
   end
   
+  def thickness_angle
+    (Math.asin((@thickness*1.0)/(2.0*@radius)))*180.0/Math::PI
+  end
+  
+  def x3
+    (cos(@angle-thickness_angle) * @radius) + @center_x
+  end
+  
+  def y3
+    @game_window.height - (sin(@angle-thickness_angle) * @radius) - @center_y
+  end
+  
+  def x4
+    (cos(@angle-thickness_angle+180) * @radius) + @center_x
+  end
+  
+  def y4
+    @game_window.height - (sin(@angle-thickness_angle+180) * @radius) - @center_y
+  end
+  
   def x1
-    cos(@angle) * @radius
+    (cos(@angle+thickness_angle) * @radius) + @center_x
   end
   
   def y1
-    @game_window.height - (sin(@angle) * @radius)
+    @game_window.height - (sin(@angle+thickness_angle) * @radius) - @center_y
   end
   
   def x2
-    cos(@angle+180) * @radius
+    (cos(@angle+180+thickness_angle) * @radius) + @center_x
   end
   
   def y2
-    @game_window.height - sin(@angle+180) * @radius
+    @game_window.height - (sin(@angle+180+thickness_angle) * @radius) - @center_y
   end
   
   def sin(angle)
@@ -48,5 +91,6 @@ class Lever
     Math.cos(angle*Math::PI/180).round(5)
   end
   
+
   
 end
